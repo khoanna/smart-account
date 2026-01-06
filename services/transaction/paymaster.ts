01f23d962a8f634f370840dbdb3ea5db2c30c31e
khoanna <khoa210105@gmail.com>
2026-01-06 22:35:01 +0700

    fix: timeout error whitelist

--- services/transaction/paymaster.ts
+++ services/transaction/paymaster.ts
@@ -1,21 +1,16 @@
-import { SPONSOR_PAYMASTER_ADDRESS } from "@/utils/constant";
+import { publicClient, SPONSOR_PAYMASTER_ADDRESS } from "@/utils/constant";
 
-import { createPublicClient, createWalletClient, encodeFunctionData, http, parseAbi, parseEther, type Hex } from "viem";
+import { createWalletClient, encodeFunctionData, http, parseAbi, parseEther, type Hex } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
 import { sepolia } from "viem/chains";
-
-const publicClient = createPublicClient({
-  transport: http(process.env.SEPOLIA_RPC_URL!),
-  chain: sepolia,
-});
 
 export const acceptUser = async (address: Hex) => {
   if (!process.env.ADMIN_PRIVATE_KEY) {
     throw new Error("ADMIN_PRIVATE_KEY environment variable is not set");
   }
-  
+
   const adminAccount = privateKeyToAccount(process.env.ADMIN_PRIVATE_KEY as Hex);
-  
+
   const adminWalletClient = createWalletClient({
     account: adminAccount,
     chain: sepolia,
@@ -29,5 +24,10 @@ export const acceptUser = async (address: Hex) => {
       args: [address],
     }),
   });
-  await publicClient.waitForTransactionReceipt({ hash: whitelistHash });
+  await publicClient.waitForTransactionReceipt({
+    hash: whitelistHash,
+    timeout: 60000,
+    confirmations: 1,
+    pollingInterval: 1000,
+  });
 };
